df4db93de8de15d085edc4a11c20399ef9eeea0f
Wael Antar <antarwael189@gmail.com>
2024-10-14 21:26:17 +0100

    6.5 Complete the nav design

--- WeatherApp/src/app/components/shared/nav/nav.component.ts
+++ WeatherApp/src/app/components/shared/nav/nav.component.ts
@@ -1,23 +1,32 @@
 import { Component } from '@angular/core';
 import { LanguageService } from '../../../services/language.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
   constructor(private languageService: LanguageService) {}
 
+  isDropdownOpen = false;
+
+  
+
   get currentLanguage(): string {
     return this.languageService.getCurrentLanguage();
   }
 
-  toggleLanguage(): void {
-    const newLang = this.currentLanguage === 'en' ? 'it' : 'en';
-    this.languageService.setLanguage(newLang);
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectLanguage(language: string): void {
+    this.languageService.setLanguage(language);
+    this.isDropdownOpen = false;
   }
   }
 
