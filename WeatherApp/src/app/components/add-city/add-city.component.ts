188c46ba3c1bbfc85c6179b7b5eaf3c3eb2f4451
Wael Antar <antarwael189@gmail.com>
2024-10-15 01:03:39 +0100

    7.2 enhance design and add translation

--- WeatherApp/src/app/components/add-city/add-city.component.ts
+++ WeatherApp/src/app/components/add-city/add-city.component.ts
@@ -2,18 +2,19 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { AddCity } from '../../constants/add-city.constant';
+import { TranslateModule } from '@ngx-translate/core';
 @Component({
   selector: 'app-add-city',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule,FormsModule,TranslateModule],
   templateUrl: './add-city.component.html',
   styleUrl: './add-city.component.scss'
 })
 export class AddCityComponent {
   cityName: string = '';
   searchResults: any[] = [];
-
+  addCitySearch:string=AddCity.searchForCity;
   @Output() cityAdded = new EventEmitter<string>();
 
   constructor(private weatherService: WeatherService) { }
