a2f9d6a46d9d7f6afef56f1a231cfa670a89ff9d
Wael Antar <antarwael189@gmail.com>
2024-10-16 11:30:32 +0100

    12.2 Add navbar atimation

--- WeatherApp/tailwind.config.js
+++ WeatherApp/tailwind.config.js
@@ -32,7 +32,15 @@ module.exports = {
       backgroundImage: {
         'custom-bg': "url('/WeatherApp/public/assets/2_3.jpg')",
       },
-      
+      keyframes: {
+        weatheria: {
+          '0%, 100%': { color: '#3b82f6', textShadow: '0px 0px 20px rgba(59, 130, 246, 0.5)' }, // Blue color for clear sky
+          '50%': { color: '#facc15', textShadow: '0px 0px 30px rgba(234, 179, 8, 0.8)' }, // Yellow for sunlight
+        },
+      },
+      animation: {
+        weatheria: 'weatheria 3s infinite alternate',
+      },
     },
   },
   plugins: [],
